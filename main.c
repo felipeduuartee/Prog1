368430fe563683f13ec5040ec95c66a2d03f82d6
felipe <felipeduarte@ufpr.br>
2023-08-06 14:02:35 -0300

    alteracoes

--- main.c
+++ main.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 
-int fibonacci(int n) {
-	if (n <= 0) {
-			return 0;
-	}
-	else if (n == 1 || n == 2) {
-			return 1;
-	}
-	else {
-			int fib1 = 1, fib2 = 1, fib = 0;
-			for (int i = 3; i <= n; i++) {
-					fib = fib1 + fib2;
-					fib1 = fib2;
-					fib2 = fib;
-			}
-			return fib;
-	}
-}
-	
 int main() {
-	int n;
-	printf("Digite um numero: ");
+	int n, reverso = 0, temp, original;
+
+	printf("Digite um numero inteiro: ");
 	scanf("%d", &n);
-	printf(" O %d numero da seuquencia de fibonacci é: %d\n", n, fibonacci(n));
-	return 0;
-}
+
+	original = n;
+
+	while (n != 0) {
+		temp = n % 10;
+		reverso = reverso * 10 + temp;
+		n /= 10;
+	}
+} 
