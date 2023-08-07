fddc0b51570546fcdec7cf15449a617bf01c0d7f
felipe <felipeduarte@ufpr.br>
2023-08-07 18:33:30 -0300

    tirando os ponteiros

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
-// INTERCALAR VETORES                                       
+//Intercalar vetores
+
 #include <stdio.h>
 
-void intercalar(int* vetor1, int* vetor2, int* vetorIntercalado, int n) {
+void intercalar(int vetor1[], int vetor2[], int vetorIntercalado[], int n) {
     for (int i = 0; i < n; i++) {
         vetorIntercalado[2*i] = vetor1[i];
         vetorIntercalado[2*i + 1] = vetor2[i];
@@ -13,7 +14,7 @@ int main() {
     int vetor2[4] = {12, 98, 90, 11};
     int vetorIntercalado[8];
 
-    intercalar(vetor1, vetor2, vetorIntercalado, 3);
+    intercalar(vetor1, vetor2, vetorIntercalado, 4);
 
     printf("Vetor intercalado: ");
     for (int i = 0; i < 8; i++) {
@@ -22,4 +23,3 @@ int main() {
     printf("\n");
     return 0;
 }
-
