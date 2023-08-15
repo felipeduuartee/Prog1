c976f70ede0b91d316cc435e5136bd6a73a75c0b
felipe <felipeduarte@ufpr.br>
2023-08-15 18:44:21 -0300

    commits

--- main.c
+++ main.c
@@ -7,13 +7,13 @@ int main() {
     int valor_quadrado = 4;
 
     printf("Digite uma sequencia de numeros (termina com 0): ");
-    
+
     // Leia o primeiro valor
     scanf("%d", &val);
     while(val != 0) {
         // Leia o próximo valor (que deve ser o quadrado do anterior)
         scanf("%d", &quad);
-        
+
         // Se o quadrado não é o quadrado do valor, então a sequência não é uma série de valor-quadrado
         if(quad != val * val) {
             valor_quadrado = 0;
@@ -22,8 +22,8 @@ int main() {
         // Leia o próximo valor
         scanf("%d", &val);
     }
-    
+
     printf("%d\n", valor_quadrado);
-    
+
     return 0;
 }
