6ac46e8013c19b6e589973c53fe1f42e4f812b45
felipe <felipeduarte@ufpr.br>
2023-09-05 19:21:11 -0300

    12

--- listaPonteiros/questao_12.c
+++ listaPonteiros/questao_12.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-void ordenados (int *a, int *b, int *c) {
+void ordena_e_verifica (int *a, int *b, int *c) {
 
-    
+    int temp;
+
+    if (*a > *b) {
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+
+    if (*a > *c) {
+        temp = *a;
+        *a = *c;
+        *c = temp;
+    }
+
+    if (*b > *c) {
+        temp = *b;
+        *b = *c;
+        *c = temp;
+    }
+
+    return (a == b && b == c) ? 1 : 0;
+
 }
