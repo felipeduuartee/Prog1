a324def45459c9e5e5323c33b467b7641c8bed7f
felipe <felipeduarte@ufpr.br>
2023-09-08 17:01:06 -0300

    att

--- exerciciosEmAula/matrizes/main.c
+++ exerciciosEmAula/matrizes/main.c
@@ -2,16 +2,32 @@
 #include "matriz.h"
 
 int main() {
-    float A[MAX][MAX];
+    float A[MAX][MAX], B[MAX][MAX], Resultado[MAX][MAX];
 
     srand(0);
 
+    // Preenchendo as matrizes A e B aleatoriamente
     preencherAleatoriamente(A);
+    preencherAleatoriamente(B);
 
-    printf("Matriz A (preenchida aleatoriamente):\n");
+    // Somando matrizes
+    somarMatrizes(A, B, Resultado);
+    printf("Resultado da Soma:\n");
     for (int i = 0; i < MAX; i++) {
         for (int j = 0; j < MAX; j++) {
-            printf("%.2f ", A[i][j]);
+            printf("%.2f ", Resultado[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    // Multiplicando matrizes
+    multiplicarMatrizes(A, B, Resultado);
+    printf("Resultado da Multiplicação:\n");
+    for (int i = 0; i < MAX; i++) {
+        for (int j = 0; j < MAX; j++) {
+            printf("%.2f ", Resultado[i][j]);
         }
         printf("\n");
     }
