3fd850728eafe415ed2e63ae95e3120f0c187d2a
Felipe Duarte <129008322+felipeduuartee@users.noreply.github.com>
2023-11-20 10:39:00 -0300

    Update ordenacao.h

--- Algoritmos 2/Trabalho 2/ordenacao.h
+++ Algoritmos 2/Trabalho 2/ordenacao.h
@@ -19,17 +19,37 @@ unsigned int getGRR();
 // O retorno das funções de ordenação é o número de comparações
 // entre elementos do vetor
 
+void swap(int *a, int *b);
+
+void preencherVetorAleatorio(int* vetor, int tamVetor);
+
+void preencherVetorCountingSort(unsigned int vetor[], int tam);
+
+void copiar(int v[], int u[], int a, int b);
+
+void merge(int vetor[], int inicio, int meio, int fim, long* numComparacoes);
+
+unsigned long mergeSortRecursive(int vetor[], int a, int b, long* numComparacoes);
+
 unsigned long mergeSort(int vetor[], int tam);
+
+unsigned long quickSortRecursivo(int vetor[], int inicio, int fim, long* numComparacoes);
 
 unsigned long quickSort(int vetor[], int tam);
 
+long particionar(int vetor[], int inicio, int fim, long* numComparacoes);
+
 unsigned long heapSort(int vetor[], int tam);
 
-void countingSort(unsigned int vetor[], int tam,
-                  int maxVal);  // maxVal é o k do algoritmo
+unsigned long maxHeapify(int vetor[], int tam, int i, unsigned long* numComparacoes);
+
+void countingSort(unsigned int vetor[], int tam,int maxVal);  // maxVal é o k do algoritmo
 
 // Na função algoritmoExtra implemente o algoritmo extra solicitado.
 // Se o seu algoritmo escolhido não faz comparações, retorne -1.
 unsigned long algoritmoExtra(int vetor[], int tam);
 
+void inserirBucket(int *bucket, int *tamanho, int valor, unsigned long *comparacoes);
+
+void insertionSort(int *arr, int tamanho, unsigned long *comparacoes);
 #endif  // ORDENACAO_H_
